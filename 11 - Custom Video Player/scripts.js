6988315ee9d79977bedaf01d791122b1b3b64f44
DarkMG73 <levelthreeemail@gmail.com>
2018-12-14 11:41:08 -0500

    Completed Lesson

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,74 @@
+
+
+// Get elements on the page
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = document.querySelector('.fullscreen');
+
+// Build functions
+function togglePlay(){
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+console.log(video);
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+let screenFull = false;
+function screenSize() {
+    screenFull = !screenFull;
+    if( screenFull ) {
+        player.style.maxWidth = 'none';
+        player.style.width = '100%';
+    } else {
+        player.style.maxWidth = '';
+        player.style.width = '';
+    }
+
+}
+// Hook up event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mouseDown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mouseDown && scrub(e));
+progress.addEventListener('mousedown', () => mouseDown = true);
+progress.addEventListener('mouseup', () => mouseDown = false);
+fullScreen.addEventListener('click', screenSize);
